cab2c80e9d4d576b36ea8ee3e81f32ec88d54615
Milko Daskalov <milko.daskalov@mpay24.com>
2018-09-24 09:40:46 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,69 @@
+# postman-test-suite
+
+## Global pre-reqquest scripts
+
+References:
+
+- [Postman sandbox](https://www.getpostman.com/docs/v6/postman/scripts/postman_sandbox)
+- [Parameter check (chai)](http://www.chaijs.com/api/bdd/)
+- [Lodash documentation](https://lodash.com/docs/4.17.10)
+
+usage:
+
+```
+let utils = eval(globals.loadUtils);
+utils.parseSOAPResponse("etp:AcceptPayment");
+```
+
+Default variables which could be set before calling parseSOAPResponse:
+
+`utils.expNs`: expected response soap namespace (default: SOAP-ENV)
+`utils.expStatus`: expected response http status code (default: 200)
+`utils.expContent`: expected response content type: (default: text/xml)
+
+Variables set after calling parseSOAPResponse:
+`utils.responseType` - contains the type (string) of the response
+`utils.responseSubType` - contains the sub-type (string) of the response
+
+### GENERAL
+
+`parseSOAPResponse(resType, subResType)`: defines the global variables for the other functions, expect <resType> response with <subResType> sub-response
+`expect(name, expected, callback)`: check if field <node> exists in the soap body and is <expected> then call <callback> with the value
+
+### RESPONSE
+
+`expectNoResponse(name)`: check that the response field <name> does not exist
+`expectResponse(name[, value[, callback]])`: check if response field <name> exists containing <value> and call <callback> with the value
+`expectStatus(expected)`: check if the status field is <expected>
+`expectReturnCode(expected)`: check if the `returnCode` field is <expected>
+`expectMpayTID()`: check if `mpayTID` exist
+`expectLocation(substring)`: check if response.location exists contains <substring>
+`expectAll(number)`: check if `all` exists and is <number>
+`expectAllAbove(number)`: check if `all` exists and is above <number>
+`expectAllBelow(number)`: check if `all` exists and is below <number>
+
+### SUB RESPONSE
+
+`expectSubResponse(name, index, expected)`: check if sub-response parameter <name> exist at <index> and is <expected> and call <callback> with the value
+`expectSubSortedBy(name)`: Check if the sub-response is sorted by the parameter <name>
+`expectSubTStatus(expected, index)`: check if sub-response parameter `tStatus` exist at <index> and is <expected>
+`expectSubTID(expected, index)`: check if sub-response parameter `tid` exist at <index> and is <expected>
+`expectSubMPayTID(index)`: check if sub-response parameter `mPayTID` exist at <index>
+
+### PARAMETER
+
+`getParameter(name, callback)`: check if name/value parameter named <name> exist and call <callback> with the value
+`expectParameter(name, expected)`: check if name/value parameter named <name> is <expected>
+
+### ERRORS
+
+`expectFaultCode(expected)`: check if the fault code is <expected>
+`expectFaultstring(expected)`: check if the fault string is <expected>
+`expectErrNo(number)`: check if the error is <number>
+`expectErrText(expected)`: check if the error text is <expected>
+
+### SAVE
+
+`saveResponseParameter(name, variable)`: save the response parameter <name> as environment variable <variable>
+`saveMPayTID(name)`: save the mpayTID response parameter as environment variable <name> (default: MPayTID)
+`saveLocation()`: save the location response parameter as environment variable Location
---
+++ pre-req-scripts.js
@@ -0,0 +1,212 @@
+postman.setGlobalVariable(
+  "loadUtils",
+  function loadUtils(body) {
+    var _ = require("lodash");
+    let utils = {
+      expNs: "SOAP-ENV",
+      expStatus: 200,
+      expContent: "text/xml",
+      responseType: "Fault"
+    };
+
+    //GENERAL
+    utils.parseSOAPResponse = (resType, resSubType) => {
+      pm.test("Parse SOAP response", () => {
+        let content = postman.getResponseHeader("Content-Type");
+        if (resType === undefined && utils.expStatus != 500) {
+          utils.expStatus = 500;
+        }
+        pm.response.to.have.status(utils.expStatus, "Invalid http status");
+        pm.response.to.have.header("Content-Type");
+        pm.expect(content, "Missing Content-Type header").to.exist;
+        pm.expect(content, "Wrong Content-Type").to.contain(utils.expContent);
+        let responseJSON = xml2Json(pm.response.text());
+        pm.expect(responseJSON, "Cannot parse the response to JSON").to.exist;
+        let soapEnvelope = responseJSON[`${utils.expNs}:Envelope`];
+        pm.expect(soapEnvelope, `${utils.expNs}:Envelope`).to.exist;
+        utils.soapBody = soapEnvelope[`${utils.expNs}:Body`];
+        pm.expect(utils.soapBody, `${utils.expNs}::Body`).to.exist;
+        utils.responseType =
+          resType === undefined ? `${utils.expNs}:Fault` : resType;
+        let value = _.get(utils.soapBody, utils.responseType);
+        pm.expect(value, utils.responseType).to.exist;
+        if (resSubType !== undefined) {
+          utils.responseSubType = utils.responseType + "." + resSubType;
+          let value = _.get(utils.soapBody, utils.responseSubType);
+          pm.expect(value, utils.responseSubType).to.exist;
+        }
+      });
+    };
+    utils.expect = (name, expected, callback) => {
+      pm.expect(name, "expect: name argument").to.exist;
+      let value = _.get(utils.soapBody, name);
+      if (expected instanceof RegExp) {
+        pm.test(`${name} matches: ${expected}`, () => {
+          pm.expect(value).to.match(expected);
+        });
+      } else if (expected !== undefined) {
+        pm.test(`${name} is: ${expected}`, () => {
+          pm.expect(value).to.equal(expected);
+        });
+      } else if (callback === undefined) {
+        pm.test(`${name} exist`, () => {
+          pm.expect(value).to.exist;
+        });
+      }
+      if (typeof callback === "function") {
+        callback(value);
+      }
+    };
+
+    //RESPONSE
+    utils.expectNoResponse = name => {
+      pm.expect(name, "expectResponse name argument").to.exist;
+      pm.expect(utils.responseType, "responseType").to.exist;
+      let paramName = `${utils.responseType}.${name}`;
+      let value = _.get(utils.soapBody, paramName);
+      pm.test(`${paramName} does not exist`, () => {
+        pm.expect(value).to.not.exist;
+      });
+    };
+    utils.expectResponse = (name, expected, callback) => {
+      utils.expect(`${utils.responseType}.${name}`, expected, callback);
+    };
+    utils.expectStatus = expected => {
+      utils.expectResponse("status", expected);
+    };
+    utils.expectReturnCode = expected => {
+      utils.expectResponse("returnCode", expected);
+    };
+    utils.expectMPayTID = () => {
+      utils.expectResponse("mpayTID");
+    };
+    utils.expectLocation = substring => {
+      utils.expectResponse("location", undefined, locationValue => {
+        if (substring !== undefined) {
+          pm.test(`Location contains: ${substring}`, () => {
+            pm.expect(locationValue).to.include(substring);
+          });
+        }
+      });
+    };
+    utils.expectAll = number => {
+      utils.expectResponse("all", String(number));
+    };
+    utils.expectAllAbove = number => {
+      pm.expect(number, "expectAllAbove number argument").to.exist;
+      utils.expectResponse("all", undefined, allValue => {
+        if (number !== undefined) {
+          pm.test(`${utils.responseType}.all is bigger than ${number}`, () => {
+            pm.expect(Number(allValue)).to.be.above(Number(number));
+          });
+        }
+      });
+    };
+    utils.expectAllBelow = number => {
+      pm.expect(number, "expectAllBelow number argument").to.exist;
+      utils.expectResponse("all", undefined, allValue => {
+        if (number !== undefined) {
+          pm.test(`${utils.responseType}.all is smaller than ${number}`, () => {
+            pm.expect(Number(allValue)).to.be.below(Number(number));
+          });
+        }
+      });
+    };
+
+    //SUB RESPONSE
+    utils.expectSubResponse = (name, index, expected, callback) => {
+      pm.expect(name, "expectSubResponse name argument").to.exist;
+      pm.expect(utils.responseSubType, "responseSubType").to.exist;
+      var paramName;
+      if (index === undefined) {
+        paramName = `${utils.responseSubType}.${name}`;
+      } else {
+        let values = _.get(utils.soapBody, utils.responseSubType);
+        pm.expect(values, utils.responseSubType).to.be.an("array");
+        pm.expect(index, "index").to.be.at.least(1);
+        pm.expect(index, "index").to.be.at.most(values.length);
+        paramName = `${utils.responseSubType}[${index - 1}].${name}`;
+      }
+      utils.expect(paramName, expected, callback);
+    };
+    utils.expectSubSortedBy = name => {
+      pm.expect(name, "expectSubSortedBy name argument").to.exist;
+      pm.test(`${utils.responseSubType} is sorted by ${name}`, () => {
+        let values = _.get(utils.soapBody, utils.responseSubType);
+        let sorted = _.sortBy(values, name);
+        let isEqual = _.isEqual(values, sorted);
+        pm.expect(values, utils.responseSubType).to.be.an("array");
+        pm.expect(isEqual).to.be.true;
+      });
+    };
+    utils.expectSubTStatus = (expected, index) => {
+      utils.expectSubResponse("tStatus", index, expected);
+    };
+    utils.expectSubTID = (expected, index) => {
+      utils.expectSubResponse("tid", index, expected);
+    };
+    utils.expectSubMPayTID = index => {
+      utils.expectSubResponse("mpayTID", index);
+    };
+
+    //PARAMETER
+    utils.getParameter = (name, callback) => {
+      pm.expect(name, "getParameter name argument").to.exist;
+      pm.expect(utils.responseType, "responseType").to.exist;
+      let valuesName = `${utils.responseType}.parameter`;
+      let values = _.get(utils.soapBody, valuesName);
+      pm.expect(values, valuesName).to.be.an("array");
+      var index;
+      let param = values.find((el, ix) => {
+        index = ix;
+        return el.name === name;
+      });
+      pm.expect(param, valuesName).to.exist;
+      pm.expect(param.value, valuesName).to.exist;
+      if (typeof callback === "function") {
+        callback(`${utils.responseType}.parameter[${index}]`);
+      }
+    };
+    utils.expectParameter = (name, expected) => {
+      utils.getParameter(name, valueName => {
+        utils.expect(`${valueName}.value`, expected);
+      });
+    };
+
+    //ERRORS
+    utils.expectFaultCode = expected => {
+      utils.expectResponse("faultcode", expected);
+    };
+    utils.expectFaultstring = expected => {
+      utils.expectResponse("faultstring", expected);
+    };
+    utils.expectErrNo = expected => {
+      utils.expectResponse("errNo", String(expected));
+    };
+    utils.expectErrText = expected => {
+      utils.expectResponse("errText", expected);
+    };
+
+    //SAVE
+    utils.saveResponseParameter = (name, variable) => {
+      pm.expect(name, "saveResponseParameter name argument").to.exist;
+      pm.expect(variable, "saveResponseParameter variable argument").to.exist;
+      pm.expect(utils.responseType, "responseType").to.exist;
+      postman.clearEnvironmentVariable(name);
+      pm.test(`Saving ${name} as ${variable}`, () => {
+        let paramName = `${utils.responseType}.${name}`;
+        let value = _.get(utils.soapBody, paramName);
+        pm.expect(value).to.exist;
+        postman.setEnvironmentVariable(variable, value);
+      });
+    };
+    utils.saveMPayTID = name => {
+      let variable = name !== undefined ? name : "MPayTID";
+      utils.saveResponseParameter("mpayTID", variable);
+    };
+    utils.saveLocation = () => {
+      utils.saveResponseParameter("location", "Location");
+    };
+    return utils;
+  } + "; loadUtils();"
+);
